347ac6d236e3457cda70216d08e29453f10a135c
RediGt <red.i@inbox.lv>
2020-07-29 17:32:22 +0300

    Ver_07.28_01

--- BrowserRM/BrowserRM.Designer.cs
+++ BrowserRM/BrowserRM.Designer.cs
@@ -34,7 +34,12 @@
             this.toolStripAddressBar = new System.Windows.Forms.ToolStripTextBox();
             this.toolStripButtonBack = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonForward = new System.Windows.Forms.ToolStripButton();
+            this.BrowserTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolStripButtonAddTab = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
+            this.BrowserTabs.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -43,7 +48,8 @@
             this.toolStripButtonGo,
             this.toolStripAddressBar,
             this.toolStripButtonBack,
-            this.toolStripButtonForward});
+            this.toolStripButtonForward,
+            this.toolStripButtonAddTab});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(800, 25);
@@ -65,6 +71,7 @@
             this.toolStripAddressBar.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.toolStripAddressBar.Name = "toolStripAddressBar";
             this.toolStripAddressBar.Size = new System.Drawing.Size(300, 25);
+            this.toolStripAddressBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripAddressBar_KeyDown);
             // 
             // toolStripButtonBack
             // 
@@ -86,16 +93,60 @@
             this.toolStripButtonForward.Text = "->";
             this.toolStripButtonForward.Click += new System.EventHandler(this.toolStripButtonForward_Click);
             // 
+            // BrowserTabs
+            // 
+            this.BrowserTabs.Controls.Add(this.tabPage1);
+            this.BrowserTabs.Controls.Add(this.tabPage2);
+            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowserTabs.Location = new System.Drawing.Point(0, 25);
+            this.BrowserTabs.Name = "BrowserTabs";
+            this.BrowserTabs.SelectedIndex = 0;
+            this.BrowserTabs.Size = new System.Drawing.Size(800, 425);
+            this.BrowserTabs.TabIndex = 1;
+            this.BrowserTabs.Click += new System.EventHandler(this.BrowserTabs_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 399);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "+";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStripButtonAddTab
+            // 
+            this.toolStripButtonAddTab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonAddTab.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAddTab.Image")));
+            this.toolStripButtonAddTab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAddTab.Name = "toolStripButtonAddTab";
+            this.toolStripButtonAddTab.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonAddTab.Text = "+";
+            this.toolStripButtonAddTab.Click += new System.EventHandler(this.toolStripButtonAddTab_Click);
+            // 
             // Browser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BrowserTabs);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Browser";
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.BrowserTabs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +159,10 @@
         private System.Windows.Forms.ToolStripTextBox toolStripAddressBar;
         private System.Windows.Forms.ToolStripButton toolStripButtonBack;
         private System.Windows.Forms.ToolStripButton toolStripButtonForward;
+        private System.Windows.Forms.TabControl BrowserTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAddTab;
     }
 }
 
--- BrowserRM/BrowserRM.cs
+++ BrowserRM/BrowserRM.cs
@@ -13,49 +13,49 @@ using CefSharp.WinForms;
 namespace BrowserRM
 {
     public partial class Browser : Form
-    {
+    {   
+        private ChromiumWebBrowser browser;
+
         public Browser()
         {
             InitializeComponent();
             InitializeBrowser();
+            InitializeForm();
         }
 
-        private ChromiumWebBrowser browser;
+        public void InitializeForm()
+        {
+            BrowserTabs.Height = ClientRectangle.Height - toolStrip1.Height;           
+        }
+
+
 
         public void InitializeBrowser()
         {
             CefSettings settings = new CefSettings();
             // Initialize cef with the provided settings
             Cef.Initialize(settings);
-            // Create a browser component
-            browser = new ChromiumWebBrowser("https://www.meteo.lv/");
-            // Add it to the form and fill it to the form window.
-            this.Controls.Add(browser);
-            browser.Dock = DockStyle.Fill;
-            browser.AddressChanged += Browser_AddressChanged;
+            
+            //BrowserTabs.TabPages[0].Dispose();
+            BrowserTabs.TabPages[0].Dispose();
+            AddBrowserTab();
         }
 
         private void toolStripButtonGo_Click(object sender, EventArgs e)
         {
-            try
-            {
-                 browser.Load(toolStripAddressBar.Text);
-            }
-            catch
-            {
-
-            }
+            Navigate(toolStripAddressBar.Text);         
         }
 
         private void toolStripButtonBack_Click(object sender, EventArgs e)
         {
-            browser.Back();
-            
+            var selectedBrowser = (ChromiumWebBrowser)BrowserTabs.SelectedTab.Controls[0];
+            selectedBrowser.Back();          
         }
 
         private void toolStripButtonForward_Click(object sender, EventArgs e)
         {
-            browser.Forward();
+            var selectedBrowser = (ChromiumWebBrowser)BrowserTabs.SelectedTab.Controls[0];
+            selectedBrowser.Forward();
         }
 
         private void Browser_AddressChanged(object sender, AddressChangedEventArgs e)
@@ -66,5 +66,68 @@ namespace BrowserRM
                 toolStripAddressBar.Text = e.Address;
             }));
         }
+
+        private void toolStripAddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Navigate(toolStripAddressBar.Text);
+            }
+        }
+
+        private void Navigate(string address)
+        {
+            try
+            {
+                var selectedBrowser = (ChromiumWebBrowser) BrowserTabs.SelectedTab.Controls[0];
+                selectedBrowser.Load(address);              
+            }
+            catch
+            {
+            }
+        }
+
+        private void toolStripButtonAddTab_Click(object sender, EventArgs e)
+        {
+            AddBrowserTab();
+            BrowserTabs.SelectedTab = BrowserTabs.TabPages[BrowserTabs.TabPages.Count - 2];
+        }
+
+        private void AddBrowserTab()
+        {
+            //adding a tab
+            var newTabPage = new TabPage();
+            newTabPage.Text = "New Tab";
+
+            BrowserTabs.TabPages.Insert(BrowserTabs.TabPages.Count - 1, newTabPage);
+            //BrowserTabs.TabPages.Add(newTabPage);
+
+            //adding browser
+            browser = new ChromiumWebBrowser("https://www.meteo.lv/");
+            browser.Dock = DockStyle.Fill;
+            browser.AddressChanged += Browser_AddressChanged;
+            browser.TitleChanged += Browser_TitleChanged;
+            newTabPage.Controls.Add(browser);
+
+        }
+
+        private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            var selectedBrowser = (ChromiumWebBrowser)sender;
+            this.Invoke(new MethodInvoker(() =>
+            {
+                selectedBrowser.Parent.Text = e.Title;
+            }));
+        }     
+
+        private void BrowserTabs_Click(object sender, EventArgs e)
+        {
+            if (BrowserTabs.SelectedTab == BrowserTabs.TabPages[BrowserTabs.TabPages.Count -1])
+            {
+                AddBrowserTab();
+                BrowserTabs.SelectedTab = BrowserTabs.TabPages[BrowserTabs.TabPages.Count - 2];
+            }
+        }
     }
+
 }
